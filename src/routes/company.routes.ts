3445c90a79654342eedcdb16dc59495ca9876a5c
Guaru34 <19170095@uttcampus.edu.mx>
2025-07-24 20:13:34 -0600

    add complete crud to company model

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -5,5 +5,8 @@ import { authenticate } from "../middleware/authenticate";
 const router = Router();
 
 router.post("/companies", authenticate, companyController.createCompany)
+router.get("/companies/:id", authenticate, companyController.getCompanyById)
+router.put("/companies/:id", authenticate, companyController.updateCompany)
+router.delete("/companies/:id", authenticate, companyController.deleteCompany)
 
 export const companyRoutes = router;
---
+++ src/utils/consts.ts
@@ -0,0 +1,5 @@
+export const USER_ACTIONS = {
+    CREACION: 'Creación',
+    ACTUALIZACION: 'Modificación',
+    DESACTIVACION: 'Desactivación',
+}
