87bc5de567fac7b85c6776f40e8e8a4d406ccbda
fedhinen <victor.adrian.ruiz.espindola@gmail.com>
2025-07-29 20:45:49 -0600

    morgan y body

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import express from 'express'
 import { router } from './routes/routes'
 import { errorHandler } from './middleware/error/handle-error'
 import cors from "cors"
+import morgan from "morgan"
+
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan("combined"))
+app.use((req, res, next) => {
+    console.log('Petición entrante: body',req.body)
+    console.log('Petición entrante: params',req.params)
+
+    next()
+})
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Server is healthy' })
 })
