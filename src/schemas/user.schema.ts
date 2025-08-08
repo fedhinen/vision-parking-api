075918a05af59c87a1858754c13040bdacfd05a8
Guaru34 <19170095@uttcampus.edu.mx>
2025-08-07 18:14:14 -0600

    feat: adjust user schema validation for usr_name and usr_password minimum length requirements

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,13 +3,13 @@ import { ERROR_CATALOG } from '../utils/error-catalog'
 
 export const userSchema = z.object({
     usr_name: z.string(ERROR_CATALOG.validation.VAL001.message)
-        .min(5, ERROR_CATALOG.validation.VAL007.message(5))
+        .min(4, ERROR_CATALOG.validation.VAL007.message(4))
         .max(30, ERROR_CATALOG.validation.VAL008.message(30)),
     usr_email: z.email(ERROR_CATALOG.validation.VAL010.message)
         .min(10, ERROR_CATALOG.validation.VAL007.message(5))
         .max(45, ERROR_CATALOG.validation.VAL008.message(45)),
     usr_password: z.string(ERROR_CATALOG.validation.VAL001.message)
-        .min(12, ERROR_CATALOG.validation.VAL007.message(12))
+        .min(10, ERROR_CATALOG.validation.VAL007.message(10))
         .max(32, ERROR_CATALOG.validation.VAL008.message(32))
         .refine((val) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,32}$/.test(
             val
